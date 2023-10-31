6d4e143793de8fa62ea17f2ca8c00bd92bd4d4fe
nomandhoni-cs <nomandhoni@yahoo.com>
2023-10-31 19:33:11 +0600

    Spreading up the components

--- src/data/images.js
+++ src/data/images.js
@@ -1,15 +1,55 @@
 const images = [
-  { id: 1, src: "/src/assets/images/image-1.webp" },
-  { id: 2, src: "/src/assets/images/image-2.webp" },
-  { id: 3, src: "/src/assets/images/image-3.webp" },
-  { id: 4, src: "/src/assets/images/image-4.webp" },
-  { id: 5, src: "/src/assets/images/image-5.webp" },
-  { id: 6, src: "/src/assets/images/image-6.webp" },
-  { id: 7, src: "/src/assets/images/image-7.webp" },
-  { id: 8, src: "/src/assets/images/image-8.webp" },
-  { id: 9, src: "/src/assets/images/image-9.webp" },
-  { id: 10, src: "/src/assets/images/image-10.jpeg" },
-  { id: 11, src: "/src/assets/images/image-11.jpeg" },
+  {
+    id: 1,
+    title: "Blue color Headphone",
+    src: "/src/assets/images/image-1.webp",
+  },
+  {
+    id: 2,
+    title: "Green color Headphone",
+    src: "/src/assets/images/image-2.webp",
+  },
+  {
+    id: 3,
+    title: "Pink color Headphone",
+    src: "/src/assets/images/image-3.webp",
+  },
+  {
+    id: 4,
+    title: "Red color Headphone",
+    src: "/src/assets/images/image-4.webp",
+  },
+  {
+    id: 5,
+    title: "Orange color Headphone",
+    src: "/src/assets/images/image-5.webp",
+  },
+  {
+    id: 6,
+    title: "White color Headphone",
+    src: "/src/assets/images/image-6.webp",
+  },
+  {
+    id: 7,
+    title: "Black color Apple Watch",
+    src: "/src/assets/images/image-7.webp",
+  },
+  {
+    id: 8,
+    title: "Orange color Apple Watch",
+    src: "/src/assets/images/image-8.webp",
+  },
+  {
+    id: 9,
+    title: "Blue color Apple Watch",
+    src: "/src/assets/images/image-9.webp",
+  },
+  { id: 10, title: "Red color Gate", src: "/src/assets/images/image-10.jpeg" },
+  {
+    id: 11,
+    title: "Yellow Color Telephone",
+    src: "/src/assets/images/image-11.jpeg",
+  },
 ];
 
 export default images;
