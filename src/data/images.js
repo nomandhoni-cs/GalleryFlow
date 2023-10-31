b09bc3691b07e47f7cdc471e418c87182f7f0765
nomandhoni-cs <nomandhoni@yahoo.com>
2023-10-31 20:24:05 +0600

    Fixed the image loading problem

--- src/data/images.js
+++ src/data/images.js
@@ -2,53 +2,53 @@ const images = [
   {
     id: 1,
     title: "Blue color Headphone",
-    src: "/src/assets/images/image-1.webp",
+    src: "../images/image-1.webp",
   },
   {
     id: 2,
     title: "Green color Headphone",
-    src: "/src/assets/images/image-2.webp",
+    src: "/images/image-2.webp",
   },
   {
     id: 3,
     title: "Pink color Headphone",
-    src: "/src/assets/images/image-3.webp",
+    src: "/images/image-3.webp",
   },
   {
     id: 4,
     title: "Red color Headphone",
-    src: "/src/assets/images/image-4.webp",
+    src: "/images/image-4.webp",
   },
   {
     id: 5,
     title: "Orange color Headphone",
-    src: "/src/assets/images/image-5.webp",
+    src: "/images/image-5.webp",
   },
   {
     id: 6,
     title: "White color Headphone",
-    src: "/src/assets/images/image-6.webp",
+    src: "/images/image-6.webp",
   },
   {
     id: 7,
     title: "Black color Apple Watch",
-    src: "/src/assets/images/image-7.webp",
+    src: "/images/image-7.webp",
   },
   {
     id: 8,
     title: "Orange color Apple Watch",
-    src: "/src/assets/images/image-8.webp",
+    src: "/images/image-8.webp",
   },
   {
     id: 9,
     title: "Blue color Apple Watch",
-    src: "/src/assets/images/image-9.webp",
+    src: "/images/image-9.webp",
   },
-  { id: 10, title: "Red color Gate", src: "/src/assets/images/image-10.jpeg" },
+  { id: 10, title: "Red color Gate", src: "/images/image-10.jpeg" },
   {
     id: 11,
     title: "Yellow Color Telephone",
-    src: "/src/assets/images/image-11.jpeg",
+    src: "/images/image-11.jpeg",
   },
 ];
 
