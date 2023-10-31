5c430d8d63730ec0eb4aaca7ddc89ef0e91fa7b7
nomandhoni-cs <nomandhoni@yahoo.com>
2023-10-31 19:12:47 +0600

    Images are displaying

--- src/data/images.js
+++ src/data/images.js
@@ -1,15 +1,15 @@
 const images = [
-  { id: 1, src: "../assets/images/image-1.webp" },
-  { id: 2, src: "../assets/images/image-2.webp" },
-  { id: 3, src: "../assets/images/image-3.webp" },
-  { id: 4, src: "../assets/images/image-4.webp" },
-  { id: 5, src: "../assets/images/image-5.webp" },
-  { id: 6, src: "../assets/images/image-6.webp" },
-  { id: 7, src: "../assets/images/image-7.webp" },
-  { id: 8, src: "../assets/images/image-8.webp" },
-  { id: 9, src: "../assets/images/image-9.webp" },
-  { id: 10, src: "../assets/images/image-10.webp" },
-  { id: 11, src: "../assets/images/image-11.webp" },
+  { id: 1, src: "/src/assets/images/image-1.webp" },
+  { id: 2, src: "/src/assets/images/image-2.webp" },
+  { id: 3, src: "/src/assets/images/image-3.webp" },
+  { id: 4, src: "/src/assets/images/image-4.webp" },
+  { id: 5, src: "/src/assets/images/image-5.webp" },
+  { id: 6, src: "/src/assets/images/image-6.webp" },
+  { id: 7, src: "/src/assets/images/image-7.webp" },
+  { id: 8, src: "/src/assets/images/image-8.webp" },
+  { id: 9, src: "/src/assets/images/image-9.webp" },
+  { id: 10, src: "/src/assets/images/image-10.jpeg" },
+  { id: 11, src: "/src/assets/images/image-11.jpeg" },
 ];
 
 export default images;
