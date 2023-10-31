c5505c0628dac732c42c519a936f740c3d66cca2
nomandhoni-cs <nomandhoni@yahoo.com>
2023-11-01 02:07:23 +0600

    Drag and Drop is Working

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,20 +1,61 @@
 import { useState } from "react";
 import images from "../../data/images";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./Gallery.css";
 
 const Gallery = () => {
   const [imagesInGallery, setImagesInGallery] = useState(images);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return; // No valid drop target
+    }
+
+    const items = Array.from(imagesInGallery);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setImagesInGallery(items);
+  };
+
   return (
-    <>
-      <div>Gallery</div>
-      {imagesInGallery.map((image) => (
-        <img
-          key={image.id}
-          className="gallery"
-          alt={image.title}
-          src={image.src}
-        />
-      ))}
-    </>
+    <div className="gallery-container">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="gallery" direction="horizontal">
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="grid"
+            >
+              {imagesInGallery.map((image, index) => {
+                return (
+                  <Draggable
+                    key={image.id}
+                    draggableId={`image-${image.id}`}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className={`grid-item ${
+                          index === 0 ? "feature-gallery" : ""
+                        }`}
+                      >
+                        <img alt={image.title} src={image.src} />
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
   );
 };
 
--- src/data/images.js
+++ src/data/images.js
@@ -1,52 +1,52 @@
 const images = [
   {
-    id: 1,
+    id: "1",
     title: "Blue color Headphone",
     src: "../images/image-1.webp",
   },
   {
-    id: 2,
+    id: "2",
     title: "Green color Headphone",
     src: "/images/image-2.webp",
   },
   {
-    id: 3,
+    id: "3",
     title: "Pink color Headphone",
     src: "/images/image-3.webp",
   },
   {
-    id: 4,
+    id: "4",
     title: "Red color Headphone",
     src: "/images/image-4.webp",
   },
   {
-    id: 5,
+    id: "5",
     title: "Orange color Headphone",
     src: "/images/image-5.webp",
   },
   {
-    id: 6,
+    id: "6",
     title: "White color Headphone",
     src: "/images/image-6.webp",
   },
   {
-    id: 7,
+    id: "7",
     title: "Black color Apple Watch",
     src: "/images/image-7.webp",
   },
   {
-    id: 8,
+    id: "8",
     title: "Orange color Apple Watch",
     src: "/images/image-8.webp",
   },
   {
-    id: 9,
+    id: "9",
     title: "Blue color Apple Watch",
     src: "/images/image-9.webp",
   },
-  { id: 10, title: "Red color Gate", src: "/images/image-10.jpeg" },
+  { id: "10", title: "Red color Gate", src: "/images/image-10.jpeg" },
   {
-    id: 11,
+    id: "11",
     title: "Yellow Color Telephone",
     src: "/images/image-11.jpeg",
   },
