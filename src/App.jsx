29f1addd8595357aabd38e030bb16839ac04395a
nomandhoni-cs <nomandhoni@yahoo.com>
2023-11-04 13:50:28 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Gallery from "./components/Gallery/Gallery";
 import photos from "./data/images.json";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const [items, setItems] = useState(photos);
   const [selectedImages, setSelectedImages] = useState([]);
   return (
     <>
+      <Navbar />
       <Gallery
         items={items}
         setItems={setItems}
